ba9a5d61ceec96eaae7d44f0003c3823796adec4
Julia Yu <jjjuliayyyu@gmail.com>
2021-08-18 20:38:51 +0200

    book

---
+++ src/main/java/pl/sda/zad22/Book.java
@@ -0,0 +1,15 @@
+package pl.sda.zad22;
+
+public class Book {
+    private String isbn;
+    private String title;
+    private String author;
+    private int year;
+
+    public Book ( String isbn, String title, String author, int year) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+}
---
+++ src/main/java/pl/sda/zad22/BookRepository.java
@@ -0,0 +1,10 @@
+package pl.sda.zad22;
+
+public class BookRepository {
+
+    private Book[] books;
+
+    public void addBook (Book book) {
+
+    }
+}
