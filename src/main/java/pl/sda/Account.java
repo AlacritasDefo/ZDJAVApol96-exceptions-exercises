5fa5462c1e4e4448d553531770d96f58f5a5104f
AlacritasDefo <arkadiuszprzychocki@gmail.com>
2021-08-18 18:33:28 +0200

    account

--- src/main/java/pl/sda/Account.java
+++ src/main/java/pl/sda/Account.java
@@ -1,2 +1,21 @@
-package pl.sda;public class Account {
+package pl.sda;
+
+public class Account {
+    private float balance;
+
+    public Account(float balance) {
+        this.balance = balance;
+    }
+
+    public void payIn(float payin) {
+        balance = balance + payin;
+    }
+
+    public void withdraw(float payout) throws AccountBalanceToLowException {
+        balance = balance - payout;
+        if (balance < payout) {
+            throw new AccountBalanceToLowException("Podana kwota wypłaty jest większa od Stanu konta");
+        }
+
+    }
 }
